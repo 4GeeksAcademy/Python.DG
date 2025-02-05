ffaa3933197bc4a0b1c2ce773684df3337e7c0ee
DianisMartinez <dianagomez192002@gmail.com>
2025-02-05 03:04:32 +0000

    no he terminado

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,99 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/registro', methods=['POST'])
+def registro():
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+
+    if not email: 
+        return jsonify({"error":"email es requerido"}), 400
+    if not password: 
+        return jsonify({"error":"password es requerido"}), 400
+    
+    
+    encontrado = User.query.filter_by(email=email).first()
+    if encontrado: 
+        return jsonify({"error":"email ya esta registrado"}), 400
+   
+    user = User()
+    user.email = email    
+    user.password = generate_password_hash(password)
+
+    db.session.add(User)
+    db.session.commit()
+    return jsonify({"mensaje":"registro exitoso, inicie sesion por favor."}), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    
+    datos = request.json
+    email = datos.get('email')
+    password = datos.get('password')
+
+    if not email:
+        return jsonify({"status": "fail", "mesage": "Email is required"}), 422
+    
+    if not password: 
+        return jsonify({"status": "fail", "mesage": "Password is required"}), 422
+    
+    found = User.query.filter_by(email=email).first()
+    
+    if not found:
+        return jsonify({"status": "fail", "message": "Usuario No Registrado"}), 404
+    
+    if not check_password_hash(found.password, password):
+        return jsonify({"status": "fail", "message": "CREDENCIALES INCORRECTAS"}), 401
+
+    access_token = create_access_token(identity=found.id, additional_claims={
+        "email": found.email,
+        "password": found.password,
+       
+    })
+
+    return jsonify({ "status": "success", "message": "login sucessfully", "access_token": access_token, "user": found.serialize()}), 200
+
+@api.route('/registro', methods=['POST'])
+def registro():
+    email = request.json.get("email")
+    firstname = request.json.get("firstname")
+    lastname = request.json.get("lastname")
+    gender = request.json.get("gender")
+    password = request.json.get("password")
+
+
+    if not email: 
+        return jsonify({"error":"email es requerido"}), 400
+    if not firstname: 
+        return jsonify({"error":"firstname es requerido"}), 400
+    if not lastname: 
+        return jsonify({"error":"lastname es requerido"}), 400
+    if not gender: 
+       return jsonify({"error":"gender es requerido"}), 400
+    if not type: 
+        return jsonify({"error":"type es requerido"}), 400
+    if not password: 
+        return jsonify({"error":"password es requerido"}), 400
+    
+    
+    encontrado = User.query.filter_by(email=email).first()
+    if encontrado: 
+        return jsonify({"error":"email ya esta registrado"}), 400
+    
+
+    user = User()
+    user.email = email
+    user.firstname = firstname
+    user.lastname = lastname
+    #user.gender = gender
+
+    #user.region = region
+    user.password = generate_password_hash(password)
+
+
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"mensaje":"registro exitoso, inicie sesion por favor."}), 200
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
+
+
+import { Login } from "./pages/Login";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+
 
 //create your first component
 const Layout = () => {
@@ -17,7 +20,10 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+
+    const token = localStorage.getItem("token");
+
 
     return (
         <div>
@@ -25,10 +31,11 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<Home />} path="/" />  
+                        <Route element={<Login />} path="/login" />
                         <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                     
+                        <Route element={<h1> Ruta no Encontrada 404!</h1>} path="*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
